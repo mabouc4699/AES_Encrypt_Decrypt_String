1e292e839d2c0dcbfc3abf77b1edca4f6efda1a3
mabouc4699 <62529043+mabouc4699@users.noreply.github.com>
2021-04-28 12:28:30 -0400

    Add files via upload

---
+++ AesEncryptDecryptString/AesEncryptDecryptString/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace AesEncryptDecryptString
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Location = new System.Drawing.Point(130, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Encrypted:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.Location = new System.Drawing.Point(130, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(312, 23);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Decrypted:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(197, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(278, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaintext:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(130, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Enter plaintext here..";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Key:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "IV:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox4.Location = new System.Drawing.Point(130, 171);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(312, 46);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox5.Location = new System.Drawing.Point(130, 223);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(312, 23);
+            this.textBox5.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(176, 99);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 19);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "128-bit";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(244, 99);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 19);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.Text = "192-bit";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(312, 99);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 19);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.Text = "256-bit";
+            this.radioButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(499, 295);
+            this.panel1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 276);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(515, 315);
+            this.Name = "Form1";
+            this.Text = "AES Encrypt/Decrypt";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AesEncryptDecryptString/AesEncryptDecryptString/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace AesEncryptDecryptString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Textbox where User enters Plaintext
+        private string plaintext;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            plaintext = textBox1.Text;
+        }
+
+        //Encrypt Button Clicked
+        private byte[] encrypted;
+        private string decOutput;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Aes myAes = Aes.Create())
+            {
+                //Code below allows user to select between 128, 192, and 256 bit encryption
+                if (radioButton1.Checked == true)
+                {
+                    myAes.KeySize = 128;
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    myAes.KeySize = 192;
+                }
+                else if (radioButton3.Checked == true)
+                {
+                    myAes.KeySize = 256;
+                }
+
+                //Generates encrypted output in format of bytes using instance of Aes Class (myAes)
+                //Also generates decrypted output in string using myAes, saved to decOutput
+                //textBox2 text is set to encrypted data (converted to string from bytes)
+                encrypted = EncryptStringToBytes(plaintext, myAes.Key, myAes.IV);
+                decOutput = DecryptStringFromBytes(encrypted, myAes.Key, myAes.IV);
+                textBox2.Text = Convert.ToBase64String(encrypted);
+            }
+        }
+
+        //Decrypt Button Clicked
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Sets textBox3 text to decOutput (decrypted output)
+            textBox3.Text = decOutput;
+        }
+
+        //When invoked, DecryptStringFromBytes returns plaintext using parameters encrypted, Key, and IV
+        private string DecryptStringFromBytes(byte[] encrypted, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            //Another instance of Aes class created, Key and IV properties set to passed parameters
+            using (Aes classAes = Aes.Create())
+            {
+                classAes.Key = Key;
+                classAes.IV = IV;
+
+                //Key and IV are displayed for Educational Purposes only (must be excluded if applied to real world applications)
+                textBox4.Text = Convert.ToBase64String(Key);
+                textBox5.Text = Convert.ToBase64String(IV);
+
+                //Creates ICryptoTransform Object decryptor that uses classAes attributes Key and IV
+                ICryptoTransform decryptor = classAes.CreateDecryptor(classAes.Key, classAes.IV);
+
+                //Code below uses msDecrypt (MemoryStream Object / Instance) to address encrypted byte[] array
+                //A cryptostream instance (csDecrypt) takes the source (msDecrypt), decryptor object, and Mode (read)
+                //A StreamReader instance (srDecrypt) takes the csDecrypt object as parameter and sets plaintext =
+                //... decryption of encrypted byte[]
+                using (MemoryStream msDecrypt = new MemoryStream(encrypted))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            //Returns plaintext indicated by method signature (private string...)
+            return plaintext;
+        }
+
+        //When invoked, EncryptStringToBytes returns ciphertext as byte[] using parameters plaintext, Key, and IV
+        private byte[] EncryptStringToBytes(string plaintext, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+
+            using (Aes classAes = Aes.Create())
+            {
+                classAes.Key = Key;
+                classAes.IV = IV;
+
+                //Key and IV are displayed for Educational Purposes only (must be excluded if applied to real world applications)
+                textBox4.Text = Convert.ToBase64String(Key);
+                textBox5.Text = Convert.ToBase64String(IV);
+
+                //Creates ICryptoTransform Object encryptor that uses classAes attributes Key and IV
+                ICryptoTransform encryptor = classAes.CreateEncryptor(classAes.Key, classAes.IV);
+
+                //Code below uses msEncrypt (MemoryStream Object / Instance) to address plaintext input
+                //A cryptostream instance (csEncrypt) takes the source (msEncrypt), encryptor object, and Mode (write)
+                //A StreamReader instance (srEncrypt) takes the csEncrypt object as parameter and sets encrypted =
+                //... decryption of encrypted byte[]
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plaintext);
+                        }
+                        //.ToArray() used because System.IO.MemoryStream does not output to byte[]
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            //Returns byte[] array encrypted indicated by method signature (private byte[]...)
+            return encrypted;
+        }
+    }
+}
